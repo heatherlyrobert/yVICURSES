dc90d77e271974b3f8b658a405bce2ca093fe9a5
heatherlyrobert <jelloshrike@gmail.com>
2023-09-10 09:49:52 -0700

    2.0w : missed old chrvisible reference

--- yVICURSES_priv.h
+++ yVICURSES_priv.h
@@ -37,7 +37,7 @@
 #define     P_VERMAJOR  "2.--, clean, improve, and expand"
 #define     P_VERMINOR  "2.0-, break out yVIKEYS into curses and opengl"
 #define     P_VERNUM    "2.0w"
-#define     P_VERTXT    "updated ySTR functions to ¶ystr¶ prefix"
+#define     P_VERTXT    "missed old chrvisible reference"
 
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
 #define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
